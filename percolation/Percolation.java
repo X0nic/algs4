174e4624a50327ab5e5b4f96a6d6f2881e791b97
Nathan Lee <nathan@globalphobia.com>
2015-01-27 16:03:46 -0700

    now purcolates

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -3,11 +3,14 @@ public class Percolation {
   private int gridSize;
   private WeightedQuickUnionUF quickUnion;
 
+  private static final int VIRTUAL_TOP = 0;
+  private static final int VIRTUAL_BOTTOM = 1;
+
   public Percolation(int N)               // create N-by-N grid, with all sites blocked
   {
-    grid = new boolean[N*N];
+    grid = new boolean[N*N+2];
     gridSize = N;
-    quickUnion = new WeightedQuickUnionUF(N*N);
+    quickUnion = new WeightedQuickUnionUF(N*N+2);
 
     // StdOut.println(grid.length);
   }
@@ -20,23 +23,35 @@ public class Percolation {
 
       if (isOpen(i-1,j))
       {
-        StdOut.println("Union Top: " + i + " " + j);
+        // StdOut.println("Union Top: " + i + " " + j);
         quickUnion.union(xyTo1D(i,j), xyTo1D(i-1,j));
       }
       if (isOpen(i+1,j))
       {
-        StdOut.println("Union Bottom: " + i + " " + j);
+        // StdOut.println("Union Bottom: " + i + " " + j);
         quickUnion.union(xyTo1D(i,j), xyTo1D(i+1,j));
       }
       if (isOpen(i,j-1))
       {
-        StdOut.println("Union Left: " + i + " " + j);
+        // StdOut.println("Union Left: " + i + " " + j);
         quickUnion.union(xyTo1D(i,j), xyTo1D(i,j-1));
       }
       if (isOpen(i,j+1))
       {
-        StdOut.println("Union Right: " + i + " " + j);
+        // StdOut.println("Union Right: " + i + " " + j);
         quickUnion.union(xyTo1D(i,j), xyTo1D(i,j+1));
+      }
+
+      if (i==1)
+      {
+        // StdOut.println("Connect to top: " + i + " " + j);
+        quickUnion.union(VIRTUAL_TOP, xyTo1D(i,j));
+      }
+
+      if (i==gridSize)
+      {
+        // StdOut.println("Connect to bottom: " + i + " " + j);
+        quickUnion.union(VIRTUAL_BOTTOM, xyTo1D(i,j));
       }
     }
   }
@@ -56,15 +71,18 @@ public class Percolation {
   {
     // return false;
 
-    return quickUnion.connected(xyTo1D(1,1), xyTo1D(i,j)) && isOpen(i,j);
+    // return quickUnion.connected(xyTo1D(1,1), xyTo1D(i,j)) && isOpen(i,j);
+    return quickUnion.connected(0, xyTo1D(i,j)) && isOpen(i,j);
   }
   
   public boolean percolates()             // does the system percolate?
   {
-    // if ( quickUnion.connected(xyTo1D(1,1), xyTo1D(gridSize,gridSize)) )
+    // if ( quickUnion.connected(VIRTUAL_TOP, VIRTUAL_BOTTOM) )
     //   StdOut.println("Purcolated");
+    
+    // return false if 
 
-    return quickUnion.connected(xyTo1D(1,1), xyTo1D(gridSize,gridSize));
+    return quickUnion.connected(VIRTUAL_TOP, VIRTUAL_BOTTOM);
   }
   
 
@@ -78,6 +96,9 @@ public class Percolation {
     int y = i-1;
 
     int coords = y * gridSize + x;
+
+    coords += 2; // Add room for virtual points.
+
     // StdOut.println( x + " " + y + " = " + coords);
 
     return coords;
